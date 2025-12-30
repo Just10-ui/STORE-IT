99cc903ff7df9b7edbfe134323fd27abbd623112
Justin <apolongjustin49@gmail.com>
2025-12-30 10:44:48 +0800

    finished the controller for product

--- backend/src/controller/productController.js
+++ backend/src/controller/productController.js
@@ -27,3 +27,48 @@ export const findProduct = async (req, res) => {
     res.status(500).json({error: 'Internal Server Error'});
   }
 };
+
+export const addProduct = async (req, res) => {
+  const { productName, type, price, amount } = req.body;
+
+  try {
+    const result = await pool.query('INSERT INTO products (name, type, price, amount) VALUES ($1, $2, $3, $4) RETURNING *;', [productName, type, price, amount]);
+
+    res.status(201).json({message: `${productName} has been added`, product: result.rows[0]});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({error: 'Internal Server Error'});
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const productId = req.params.productId;
+  const { productName, type, price, amount } = req.body;
+
+  try {
+    const updated = await pool.query('SELECT name FROM products WHERE id = $1;', [productId]);
+    const result = await pool.query('UPDATE products SET name = $1, type = $2, price = $3, amount = $4 WHERE id = $5 RETURNING *;', [productName, type, price, amount, productId]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({message: `No product with the id: ${productId}`});
+    } else {
+      return res.status(200).json({message: `${updated.rows[0].name} has been updated`, product: result.rows[0]});
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({error: 'Internal Server Error'});
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const productId = req.params.productId;
+
+  try {
+    const result = await pool.query('DELETE FROM products WHERE id = $1 RETURNING *;', [productId]);
+
+    res.status(200).json({message: `${result.rows[0].name} has been deleted`, product: result.rows[0]});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({error: 'Internal Server Error'});
+  }
+};
--- backend/src/router/productRoutes.js
+++ backend/src/router/productRoutes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { parseJson } from '../middleware/middleware.js';
-import { viewProducts, findProduct } from '../controller/productController.js';
+import { viewProducts, findProduct, addProduct, updateProduct, deleteProduct } from '../controller/productController.js';
 
 const productRoutes = Router();
 
 productRoutes.get('/', parseJson, viewProducts);
 productRoutes.get('/:productName', parseJson, findProduct);
+productRoutes.post('/add', parseJson, addProduct);
+productRoutes.put('/:productId', parseJson, updateProduct);
+productRoutes.delete('/:productId', parseJson, deleteProduct);
 
 export default productRoutes;
